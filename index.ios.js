829732cdc38a616e89df8bd27ba96115162b21d0
Jonathan Porta <jonathan@jonathanporta.com>
2017-07-07 01:48:04 -0600

    Basic MapView Working

---
+++ index.ios.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import MapView from './MapView';
+
+export default class ReactNativeModuleExample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Sex Map.
+        </Text>
+        <MapView style={styles.map}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeModuleExample', () => ReactNativeModuleExample);
---
+++ ios/ReactNativeModuleExample/RNTMapManager.m
@@ -0,0 +1,25 @@
+//
+//  RNTMapManager.m
+//  ReactNativeModuleExample
+//
+//  Created by Jonathan Porta on 7/7/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import <React/RCTViewManager.h>
+
+@interface RNTMapManager : RCTViewManager
+@end
+
+@implementation RNTMapManager
+
+RCT_EXPORT_MODULE()
+
+- (UIView *)view
+{
+  return [[MKMapView alloc] init];
+}
+
+@end
